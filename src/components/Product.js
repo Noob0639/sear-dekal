a2a51c27185c99df663c080c26fa9f19fccf6d97
Noob0639 <69603715+Noob0639@users.noreply.github.com>
2026-09-25 11:04:43 +0200

    Translate 'in cart' text to Swedish

--- src/components/Product.js
+++ src/components/Product.js
@@ -29,7 +29,7 @@ export default class Product extends Component {
                   {inCart ? (
                     <p className="text-capitalize mb-0" disabled>
                       {" "}
-                      in cart
+                      redan tillagd
                     </p>
                   ) : (
                     <i className="fas fa-cart-plus" />
