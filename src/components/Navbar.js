d03f29f708fbb6adf23eeb344324e5ccbc44081e
Noob0639 <69603715+Noob0639@users.noreply.github.com>
2026-09-25 10:44:30 +0200

    Change 'products' link text to 'dekaler'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default class Navbar extends Component {
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ms-5">
             <Link to="/" className="nav-link">
-              products
+              dekaler
             </Link>
           </li>
         </ul>
